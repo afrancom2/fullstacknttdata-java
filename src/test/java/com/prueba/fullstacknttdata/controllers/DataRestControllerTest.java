3f0843e065c6c4dcbd09cd78f06a4c208762d9b8
Andres Franco <afrancom2@gmail.com>
2025-02-24 18:20:41 -0500

    change test

--- src/test/java/com/prueba/fullstacknttdata/controllers/DataRestControllerTest.java
+++ src/test/java/com/prueba/fullstacknttdata/controllers/DataRestControllerTest.java
@@ -1,51 +1,36 @@
 package com.prueba.fullstacknttdata.controllers;
 
 import com.prueba.fullstacknttdata.services.IUserDataService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(DataRestController.class)
-@ContextConfiguration(classes = DataRestControllerTest.TestConfig.class) // Contexto personalizado
+@WebMvcTest(controllers = DataRestController.class)
 class DataRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockitoBean
     private IUserDataService userDataService;
 
+    @DisplayName("Test get user data")
     @Test
-    void userData_ShouldReturnNOk() throws Exception {
-        doNothing().when(userDataService).getUserData(1L);
+    void deleteUserData() throws Exception {
+        willDoNothing().given(userDataService).getUserData(1L);
+        ResultActions resultActions = mockMvc.perform(
+                get("/api/{id}", 1L)
+        );
 
-        mockMvc.perform(get("/api/1"))
-                .andExpect(status().is4xxClientError());
+        resultActions.andExpect(status().isOk());
     }
 
-    @Test
-    void userData_ShouldHandleException() throws Exception {
-        doThrow(new RuntimeException("Service exception")).when(userDataService).getUserData(2L);
-
-        mockMvc.perform(get("/api/2"))
-                .andExpect(status().is4xxClientError());
-    }
-
-    @Configuration
-    static class TestConfig {
-        @Bean
-        public IUserDataService userDataService() {
-            return Mockito.mock(IUserDataService.class);
-        }
-    }
 }
